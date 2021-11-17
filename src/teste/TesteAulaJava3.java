25ce72d931856022f31d1f47327dbf8c3ce7026b
Vinicius de Araujo <viniciusideal10@gmail.com>
2021-11-17 16:44:53 -0400

    mudanca pra testar

--- src/teste/TesteAulaJava3.java
+++ src/teste/TesteAulaJava3.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class TesteAulaJava3 {
     public static void main(String[] args) {
         Date data = new Date();
-        System.out.println("A data de hoje é:");
+        System.out.println("A data de hoje é: ");
         System.out.println(data);
 
     }
